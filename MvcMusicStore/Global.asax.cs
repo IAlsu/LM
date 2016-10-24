0e21594ef802c6699476529769a5620503adca3b
Alsu Islamova <Alsu_Islamova@epam.com>
2016-10-25 00:46:39 +0600

    Add logging task

---
+++ MvcMusicStore/Global.asax.cs
@@ -0,0 +1,58 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using MvcMusicStore.Controllers;
+using MvcMusicStore.Infrastructure;
+using NLog;
+using PerformanceCounterHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace MvcMusicStore
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private readonly ILogger logger;
+
+        public MvcApplication()
+        {
+            logger = LogManager.GetCurrentClassLogger();
+        }
+        protected void Application_Start()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(HomeController).Assembly);
+
+            builder.Register(f => LogManager.GetLogger("ForControllers"))
+                .As<ILogger>();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(builder.Build()));
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            logger.Info("Application starter");
+
+            using (var counterHelper = PerformanceHelper.CreateCounterHelper<Counters>("Test project"))
+            {
+                counterHelper.RawValue(Counters.GoToHome, 0);
+                counterHelper.RawValue(Counters.LogIn, 0);
+                counterHelper.RawValue(Counters.LogOff, 0);
+            }
+        }
+
+        protected void Application_Error()
+        {
+            var ex = Server.GetLastError();
+
+            logger.Error(ex.ToString());
+        }
+    }
+}
---
+++ MvcMusicStore/Infrastructure/Counters.cs
@@ -0,0 +1,33 @@
+﻿using PerformanceCounterHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Infrastructure
+{
+    [PerformanceCounterCategory
+        ("MvcMusicStore",
+        System.Diagnostics.PerformanceCounterCategoryType.MultiInstance,
+        "MvcMusicStore")]
+    public enum Counters
+    {
+        [PerformanceCounter
+        ("Go to home count",
+         "Go to home ",
+            System.Diagnostics.PerformanceCounterType.NumberOfItems32)]
+        GoToHome,
+
+        [PerformanceCounter
+        ("LogIn count",
+         "LogIn",
+            System.Diagnostics.PerformanceCounterType.NumberOfItems32)]
+                LogIn,
+
+        [PerformanceCounter
+        ("LogOff count",
+         "LogOff",
+            System.Diagnostics.PerformanceCounterType.NumberOfItems32)]
+        LogOff
+    }
+}
